a6c0e352d7c78c07e2fb05288794dd4c73dda747
2021CraBraJ <2021crabraj@share.wilsonsd.org>
2016-09-01 19:12:43 -0400

    Getting Ready
    I'm getting the site ready for adding features.

--- primary.js
+++ primary.js
@@ -1,41 +1,54 @@
 'use strict';
-///Variables
-var config = {
-  apiKey: "AIzaSyB-_oxVKECJutT52DBsyoM0cyVnv9_LHUk",
-  authDomain: "test-baf2c.firebaseapp.com",
-  databaseURL: "https://test-baf2c.firebaseio.com",
-  storageBucket: "test-baf2c.appspot.com",
-},
-provider = new firebase.auth.GoogleAuthProvider();;
-///End
+$(document).ready(function() {
+    ///Variables
+    var config = {
+            apiKey: "AIzaSyB-_oxVKECJutT52DBsyoM0cyVnv9_LHUk",
+            authDomain: "test-baf2c.firebaseapp.com",
+            databaseURL: "https://test-baf2c.firebaseio.com",
+            storageBucket: "test-baf2c.appspot.com",
+        },
+        provider = new firebase.auth.GoogleAuthProvider();
+    ///End
 
+    if (window.location.href == "http://localhost:5000/" || "https://2021crabraj.github.io/Messaging-App/") {
+        ///Firebase
+        //Initialize firebase
+        firebase.initializeApp(config);
+        //Setup authentication
+        function loginGooglePopup() {
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                var token = result.credential.accessToken;
+                var user = result.user;
+            }).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                // ...
+            });
+        }
 
-///Firebase
-//Initialize firebase
-firebase.initializeApp(config);
-//Setup authentication
-function loginGooglePopup() {
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
-  }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    // ...
-  });
-}
-function loginGoogleRedirect() {
-  firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    var token = result.credential.accessToken;
-  }
-  var user = result.user;
-  }).catch(function(error) {
-    var errorCode = error.code;
-    var email = error.email;
-    var credential = error.credential;
-  });
-}
+        function loginGoogleRedirect() {
+            firebase.auth().getRedirectResult().then(function(result) {
+                if (result.credential) {
+                    var token = result.credential.accessToken;
+                }
+                var user = result.user;
+            }).catch(function(error) {
+                var errorCode = error.code;
+                var email = error.email;
+                var credential = error.credential;
+            });
+        }
+        $("#login-button").click(function() {
+            loginGooglePopup();
+            $("#login-button").hide();
+            $("#user-menu-button").show();
+            $("#user-menu-button").text(firebase.auth().currentUser.displayName);
+        });
+        $("#user-menu-button").click(function() {
+            firebase.auth().signOut();
+        });
+    }
+});
 ///End
