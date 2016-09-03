566f3507860fea864c3db7ed4691bd120ba41672
2021CraBraJ <2021CraBraJ@share.wilsonsd.org>
2016-09-03 14:58:11 -0400

    Authentication Added
    I added authentication and the ability to save messages to the database.

--- primary.js
+++ primary.js
@@ -1,26 +1,29 @@
 'use strict';
 //Declerations
-var provider = new firebase.auth.GoogleAuthProvider();
+var provider = new firebase.auth.GoogleAuthProvider(),
+	signed_in = null;
 
 function Application() {
   //Easy access to firebase refrences
-  this.user = firebase.auth().currentUser
   
   //Get authentication buttons
   this.signInButton = document.getElementById('login-button');
   this.userMenuButton = document.getElementById('user-menu-button');
+  this.sendButton = document.getElementById('send-button');
   
   //Bind event handelers
-  this.signInButton.addEventListener('click',SignIn())
-  this.userMenuButton.addEventListener('click',SignOut())
-}
-function SignIn() {
+  this.signInButton.addEventListener('click',this.SignIn);
+  this.userMenuButton.addEventListener('click',this.SignOut);
+  this.sendButton.addEventListener('click',this.SendMessage);
+  }
+Application.prototype.SignIn = function() {
   firebase.auth().signInWithPopup(provider).then(function(result) {
   var token = result.credential.accessToken;
   var user = result.user;
   $("#login-button").hide();
   $("#user-menu-button").show();
-  $("#user-menu-button").text(this.user.displayName);
+  $("#user-menu-button").text(user.displayName);
+  $("#message-box").show();
   }).catch(function(error) {
   var errorCode = error.code;
   var errorMessage = error.message;
@@ -28,16 +31,48 @@ function SignIn() {
   var credential = error.credential;
   });
 }
-function SignOut() {
-  try {
-    firebase.auth().signOut()
+Application.prototype.SignOut = function() {
+	try {
+	  firebase.auth().signOut()
       $("#login-button").show();
       $("#user-menu-button").hide();
       $("#user-menu-button").text("");
-  } catch (e) {
-    
-  }
+	  $("#message-box").hide();
+	} catch (e) {
+		console.error(e);
+	}
 }
+Application.prototype.FirstLoginSetup = function() {
+	//TODO: add account types selection
+}
+Application.prototype.SendMessage = function() {
+	firebase.database().ref('messages/' + Date.now() +  Math.floor(Math.random() * 10001)).set({
+		message: $("#message-input").val(),
+		sender: firebase.auth().currentUser.uid
+    });
+	$("#message-input").val(null);
+}
+Application.prototype.LoadMessages = function() {
+		var messageRef = firebase.database().ref("messages");
+		messagesRef.off();
+		
+		messageRef.on('child_added', function(data) {
+			var message = data.message();
+			alert(message);
+		}
+	)
+}
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+	  signed_in = true;
+	  $("#login-button").hide();
+	  $("#user-menu-button").show();
+      $("#user-menu-button").text(user.displayName);
+	  $("#message-box").show();
+  } else {
+    signed_in = false;
+  }
+});
 window.onload = function() {
   window.Application = new Application();
 };
