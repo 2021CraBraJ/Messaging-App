629c2ed30ecd6c3117eb7cdfdc8b97595618274c
2021CraBraJ <2021crabraj@share.wilsonsd.org>
2016-09-03 20:32:11 -0400

    Messaging Added
    Add user options menu now you can log out or delete your account. When
    you are logged in the main body is a chat.

--- primary.js
+++ primary.js
@@ -1,79 +1,127 @@
+///File primary.js
+///Description the main javascript file for the Application
 'use strict';
 //Declerations
 var provider = new firebase.auth.GoogleAuthProvider(),
-	signed_in = null;
+    signed_in = null;
 
 function Application() {
-  //Easy access to firebase refrences
-  
-  //Get authentication buttons
-  this.signInButton = document.getElementById('login-button');
-  this.userMenuButton = document.getElementById('user-menu-button');
-  this.sendButton = document.getElementById('send-button');
-  
-  //Bind event handelers
-  this.signInButton.addEventListener('click',this.SignIn);
-  this.userMenuButton.addEventListener('click',this.SignOut);
-  this.sendButton.addEventListener('click',this.SendMessage);
-  }
+    //Get buttons
+    this.signInButton = document.getElementById('login-button');
+    this.userMenuButton = document.getElementById('user-menu-button');
+    this.signOutButton = document.getElementById('logout-button');
+    this.deleteAccountButton = document.getElementById('delete-account-button');
+    this.sendButton = document.getElementById('send-button');
+
+    //Bind event handelers
+    this.signInButton.addEventListener('click', this.SignIn);
+    this.userMenuButton.addEventListener('click', this.ShowUserMenu);
+    this.signOutButton.addEventListener('click', this.SignOut);
+    this.sendButton.addEventListener('click', this.SendMessage);
+    this.deleteAccountButton.addEventListener('click', this.DeleteAccount)
+}
+
+//The Application's SignIn method
 Application.prototype.SignIn = function() {
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-  var token = result.credential.accessToken;
-  var user = result.user;
-  $("#login-button").hide();
-  $("#user-menu-button").show();
-  $("#user-menu-button").text(user.displayName);
-  $("#message-box").show();
-  }).catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  var email = error.email;
-  var credential = error.credential;
-  });
-}
-Application.prototype.SignOut = function() {
-	try {
-	  firebase.auth().signOut()
-      $("#login-button").show();
-      $("#user-menu-button").hide();
-      $("#user-menu-button").text("");
-	  $("#message-box").hide();
-	} catch (e) {
-		console.error(e);
-	}
-}
-Application.prototype.FirstLoginSetup = function() {
-	//TODO: add account types selection
-}
-Application.prototype.SendMessage = function() {
-	firebase.database().ref('messages/' + Date.now() +  Math.floor(Math.random() * 10001)).set({
-		message: $("#message-input").val(),
-		sender: firebase.auth().currentUser.uid
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        $("#login-button").hide();
+        $("#user-menu-button").show();
+        $("#user-menu-button").text(user.displayName);
+        $("#message-box").show();
+        $("#messages").empty()
+        Application.LoadMessages();
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
     });
-	$("#message-input").val(null);
 }
+
+//The Application's SignOut method
+Application.prototype.SignOut = function() {
+    try {
+        firebase.auth().signOut()
+        $("#login-button").show();
+        $("#user-menu-button").hide();
+        $("#user-menu-button").text("");
+        $("#message-box").hide();
+        $("#user-menu").slideToggle();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+//The Application's DeleteAccount method
+Application.prototype.DeleteAccount = function() {
+    firebase.auth().currentUser.delete().then(function() {
+        $("#login-button").show();
+        $("#user-menu-button").hide();
+        $("#user-menu-button").text("");
+        $("#message-box").hide();
+        $("#user-menu").slideToggle();
+    }, function(error) {
+
+    });
+}
+
+//The Application's ShowUserMenu method
+Application.prototype.ShowUserMenu = function() {
+    $("#user-menu").slideToggle();
+}
+
+//The Application's FirstLoginSetup method
+Application.prototype.FirstLoginSetup = function() {
+    //TODO: add account setup form
+}
+
+//The Application's SendMessage method
+Application.prototype.SendMessage = function() {
+    firebase.database().ref('messages/' + Date.now() + Math.floor(Math.random() * 10001)).set({
+        message: $("#message-input").val(),
+        sender: firebase.auth().currentUser.uid,
+        name: firebase.auth().currentUser.displayName
+    });
+    $("#message-input").val(null);
+}
+
+//The Application's LoadMessages method
 Application.prototype.LoadMessages = function() {
-		var messageRef = firebase.database().ref("messages");
-		messagesRef.off();
-		
-		messageRef.on('child_added', function(data) {
-			var message = data.message();
-			alert(message);
-		}
-	)
+    var messageRef = firebase.database().ref("messages");
+    messageRef.off();
+
+    messageRef.limitToLast(20).on('child_added', function(data) {
+        var message = data.val();
+        Application.showMessage(message.message, message.name)
+    })
 }
+
+//The Application's showMessage method
+Application.prototype.showMessage = function(message, name) {
+    $("#messages").append("<li>(" + name + ")" + message + "</li>");
+}
+
+//The Firebase onAuthStateChanged handeler
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-	  signed_in = true;
-	  $("#login-button").hide();
-	  $("#user-menu-button").show();
-      $("#user-menu-button").text(user.displayName);
-	  $("#message-box").show();
-  } else {
-    signed_in = false;
-  }
+    if (user) {
+        signed_in = true;
+        $("#login-button").hide();
+        $("#user-menu-button").show();
+        $("#user-menu-button").text(user.displayName);
+        $("#message-box").show();
+    } else {
+        signed_in = false;
+    }
 });
+
+//Loads the app and messages on window load event
 window.onload = function() {
-  window.Application = new Application();
+    window.Application = new Application();
+    $("#messages").empty();
+    Application.LoadMessages();
 };
-///End
+
+
+///End Of File
